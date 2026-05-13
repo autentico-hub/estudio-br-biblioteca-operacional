f5957a2e2e1b0f6e02ec85cd6df4676c9126402b
autentico-hub <autentico@estudiobrpilates.com.br>
2026-05-13 18:04:50 -0300

    Update supabase-config.js

--- supabase-config.js
+++ supabase-config.js
@@ -1,4 +1,4 @@
 window.BIBLIOTECA_BR_SUPABASE = {
   url: "https://eoxrrpcdpblwzyoejrmp.supabase.co",
-  anonKey: "COLE_AQUI_A_CHAVE_PUBLICAVEL_SB_PUBLISHABLE"
+  anonKey: "sb_publishable_lK2SwAux_rOisMksPCPYaQ_oJaMZNYf"
 };
